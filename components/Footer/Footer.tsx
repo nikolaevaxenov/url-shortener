731c6af6d04676bb2d30db7a14951c2913118bbc
Ivan Nikolaev-Axenov <nikolaevaxenov@gmail.com>
2022-08-05 13:53:30 +0300

    Link to github added in footer

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -1,9 +1,14 @@
+import Link from "next/link";
 import styles from "./Footer.module.scss";
 
 export default function Footer() {
   return (
     <footer className={styles.footer}>
-      <p>© 2022 Nikolaev-Axenov</p>
+      <Link href="https://github.com/nikolaevaxenov/">
+        <a target="_blank">
+          © 2022 <u>Nikolaev-Axenov</u>
+        </a>
+      </Link>
     </footer>
   );
 }
