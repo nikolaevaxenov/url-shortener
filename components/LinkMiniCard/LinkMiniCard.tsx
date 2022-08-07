71ad479e5dc8d848ce4fb2ec9b7a05bc38731b47
Ivan Nikolaev-Axenov <nikolaevaxenov@gmail.com>
2022-08-07 23:12:31 +0300

    Left side bar in profile selection bug fixed

--- components/LinkMiniCard/LinkMiniCard.tsx
+++ components/LinkMiniCard/LinkMiniCard.tsx
@@ -1,14 +1,23 @@
 import { AiFillEye } from "react-icons/ai";
 import ILink from "../../interfaces/link";
 import styles from "./LinkMiniCard.module.scss";
+import { useAppSelector } from "../../hooks/redux";
+import classnames from "classnames";
 
 type LinkMiniCardProps = {
   link: ILink;
 };
 
 export default function LinkMiniCard({ link }: LinkMiniCardProps) {
+  const idCard = useAppSelector((state) => state.card.idCard);
+
   return (
-    <div className={styles.wrapper}>
+    <div
+      className={classnames(
+        styles.wrapper,
+        idCard === link._id && styles.wrapper__selected
+      )}
+    >
       <p className={styles.wrapper__shortLink}>goshort.ga/{link.shortLink}</p>
       <p className={styles.wrapper__fullLink}>{link.fullLink}</p>
       <div className={styles.wrapper__dateViews}>
