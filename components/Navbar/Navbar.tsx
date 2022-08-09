440b81c9657490cc940097023642aafc8cdeb206
Ivan Nikolaev-Axenov <nikolaevaxenov@gmail.com>
2022-08-09 15:15:29 +0300

    Hamburger menu styling fixed

--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -2,9 +2,8 @@ import Link from "next/link";
 import { useUser } from "@auth0/nextjs-auth0";
 import styles from "./Navbar.module.scss";
 import { useRouter } from "next/router";
-import { useRef, useState } from "react";
+import { useRef, useState, useEffect } from "react";
 import { useTranslation } from "next-i18next";
-import { GoSignOut } from "react-icons/go";
 import { Turn as Hamburger } from "hamburger-react";
 import classnames from "classnames";
 import "animate.css";
@@ -16,6 +15,34 @@ export default function Navbar() {
   const selectRef = useRef<HTMLSelectElement>(null);
   const menuDivRef = useRef<HTMLDivElement>(null);
   const [isOpen, setOpen] = useState(false);
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    const media = window.matchMedia("(max-width: 768px)");
+    if (media.matches !== isMobile) {
+      setIsMobile(media.matches);
+    }
+    const listener = () => setIsMobile(media.matches);
+    window.addEventListener("resize", listener);
+    return () => window.removeEventListener("resize", listener);
+  }, [isMobile]);
+
+  useEffect(() => {
+    console.log("isMobile = ", isMobile);
+    if (menuDivRef.current) {
+      if (!isMobile) {
+        setOpen(false);
+        menuDivRef.current.style.display = "flex";
+        menuDivRef.current.style.opacity = "100";
+      } else {
+        if (isOpen) {
+          menuDivRef.current.style.display = "flex";
+        } else {
+          menuDivRef.current.style.opacity = "0";
+        }
+      }
+    }
+  }, [isMobile, isOpen]);
 
   const handleLocaleChange = () => {
     router.push(router.route, router.asPath, {
@@ -32,56 +59,14 @@ export default function Navbar() {
           </Link>
         </div>
         <div className={styles.navbar__links}>
-          <div className={styles.navbar__desktop}>
-            <select
-              onChange={handleLocaleChange}
-              ref={selectRef}
-              value={router.locale}
-            >
-              <option value="ru">🇷🇺 Русский</option>
-              <option value="en">🇺🇸 English</option>
-            </select>
-            {user ? (
-              <>
-                <Link href="/profile">
-                  <div className={styles.profileLinks}>
-                    <a className={styles.navbar__link}>{user.email}</a>
-                  </div>
-                </Link>
-                <Link href="/api/auth/logout">
-                  <a className={styles.navbar__link}>
-                    <GoSignOut />
-                  </a>
-                </Link>
-              </>
-            ) : (
-              <>
-                <Link href="/api/auth/login">
-                  <a className={styles.navbar__link}>{t("loginRegister")}</a>
-                </Link>
-              </>
-            )}
+          <div className={styles.navbar__hamburger}>
+            <Hamburger toggled={isOpen} toggle={setOpen} />
           </div>
-        </div>
-        <div className={styles.navbar__mobile}>
-          <Hamburger
-            toggled={isOpen}
-            toggle={setOpen}
-            onToggle={(toggled) => {
-              if (menuDivRef.current) {
-                if (toggled) {
-                  menuDivRef.current.style.display = "flex";
-                } else {
-                  menuDivRef.current.style.opacity = "0";
-                }
-              }
-            }}
-          />
           <div
             ref={menuDivRef}
             className={classnames(
               styles.navbar__box,
-              "animate__animated",
+              isMobile ? "animate__animated" : "",
               isOpen ? "animate__fadeInLeft" : "animate__fadeOutLeft"
             )}
           >
--- next-i18next.config.js
+++ next-i18next.config.js
@@ -1,7 +1,9 @@
+const path = require("path");
+
 module.exports = {
   i18n: {
     defaultLocale: "ru",
     locales: ["ru", "en"],
-    localePath: "./locales",
+    localePath: path.resolve("./public/static/locales"),
   },
 };
